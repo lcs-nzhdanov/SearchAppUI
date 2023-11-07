452152ffc4c54d323ab5359d19bdce0f3664a12c
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-11-07 15:24:45 -0500

    Created a sample of a list item, going to create a custom structure using it

--- SearchAppUI/ContentView.swift
+++ SearchAppUI/ContentView.swift
@@ -9,16 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Image("witcher3")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 12.5))
+                        .padding(.horizontal, 5)
+                    
+                    VStack(spacing: 5) {
+                        HStack {
+                            Text("GAME")
+                                .bold()
+                            .foregroundStyle(.blue)
+                            
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("""
+    The Witcher 3:
+    Widld Hunt
+    """)
+                            .font(.system(size: 22))
+                            .bold()
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("2015")
+                                .font(.system(size: 20))
+                            .foregroundStyle(Color("Grey"))
+                            
+                            Spacer()
+                        }
+                    }
+                }
+                .frame(height: 125)
+                
+                
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
