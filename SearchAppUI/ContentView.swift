1389c64fc47b2923fc5b38138b5107eaa9591793
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-11-07 14:56:29 -0500

    Initial Commit

---
+++ SearchAppUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchAppUI
+//
+//  Created by Nikita Zhdanov on 2023-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchAppUI/SearchAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppUIApp.swift
+//  SearchAppUI
+//
+//  Created by Nikita Zhdanov on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
